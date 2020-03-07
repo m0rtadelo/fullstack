e08c5a3212afde111043292573c4db1bd9e78e25
m0rtadelo <ricard.figuls@gmail.com>
2020-03-07 22:32:56 +0100

    ADD docker mongodb service with basic init

---
+++ mongo-init.js
@@ -0,0 +1,26 @@
+db.createUser(
+        {
+            user: "user",
+            pwd: "password",
+            roles: [
+                {
+                    role: "readWrite",
+                    db: "fullstack"
+                }
+            ]
+        }
+);
+db.roles.insertMany([
+  { name: "admin", admin: true, read: true, write: true },
+  { name: "user", admin: false, read: true, write: true },
+  { name: "ro", admin: false, read: true, write: false }
+]);
+db.users.insert({ user: "admin", pwd: "", roles: ["admin"] });
+db.boxes.insertMany([
+{ name: "BOX01", description: "Big box blue" },
+{ name: "BOX02", description: "Big box red" }
+]);
+db.items.insertMany([
+{ name: "House music vol.1", box: "BOX01", description: "House music live volumen 1" },
+{ name: "House music vol.2", box: "BOX01", description: "House music live volumen 2" }
+]);
