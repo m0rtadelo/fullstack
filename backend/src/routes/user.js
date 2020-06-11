287a28b6cc230425db72d754200726a394e95d64
Ricard Fíguls Mateu <ricard.figuls@gmail.com>
2020-06-11 21:42:39 +0200

    ADD query endpoint

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -1,16 +1,21 @@
 import express from 'express';
-import utils from '../utils';
 
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-  if (utils.isAuthorized(req, res)) {8
     return res.send(Object.values(await req.context.models.users.get()));
-  }
 });
 
+router.post('/', async (req, res) => {
+    let item;
+    try {
+      item = Object.values(await req.context.models.users.find(req.body));
+    } catch (error) {}
+    if(utils.isValidItem(item, res)) {
+      return res.send(item);
+    }
+})
 router.get('/:userId', async (req, res) => {
-  if (utils.isAuthorized(req, res)) {
     let item;
     try {
       item = Object.values(await req.context.models.users.get(req.params.userId));
@@ -18,7 +23,6 @@ router.get('/:userId', async (req, res) => {
     if(utils.isValidItem(item, res)) {
       return res.send(item);
     }
-  }
 });
 
 export default router;
